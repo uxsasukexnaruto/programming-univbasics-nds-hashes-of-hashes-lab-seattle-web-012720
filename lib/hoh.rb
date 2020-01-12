89353d45a9d08fe3797e8e71e89b42653ced7384
Ha Nguyen <nkei1908@yahoo.com>
2020-01-12 04:15:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -57,9 +57,7 @@ def naming_system
               label: "Genus",
               sub_category: {
                 label: "Species",
-                sub_category: {
-                  label: nil
-                }
+                sub_category: nil
               }
             }
           }
